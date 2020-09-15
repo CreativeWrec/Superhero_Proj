ceb941baef8063b5d996e9cb1c9fb13e344c671a
Kevin Harvey Jr <lordty@gmail.com>
2020-09-15 13:44:17 -0500

    Added_Migration_&_Updated_Database

--- Superhero_Project/Data/ApplicationDbContext.cs
+++ Superhero_Project/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Superhero_Project.Models;
 
 namespace Superhero_Project.Data
 {
@@ -11,6 +12,8 @@ namespace Superhero_Project.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
         }
+        public DbSet<Superhero> superheroes { get; set; }
     }
 }
---
+++ Superhero_Project/Data/Migrations/20200915183655_InitialMigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Superhero_Project.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "superheroes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(nullable: true),
+                    alterEgo = table.Column<string>(nullable: true),
+                    primaryAbility = table.Column<string>(nullable: true),
+                    secondaryAbility = table.Column<string>(nullable: true),
+                    catchphrase = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_superheroes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "superheroes");
+        }
+    }
+}
